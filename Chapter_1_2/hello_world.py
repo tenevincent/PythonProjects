e270e815fbfb2b71d86d246f75fbf5f0bbedc5fa
vincent tene <vincent.tene@gmail.com>
2020-04-28 16:36:27 +0200

    chapter 2-3

---
+++ Chapter_1_2/hello_world.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 28 15:07:29 2020
+
+@author: Tene
+"""
+
+
+print("Hello Python world!")
+message = "Hello Python world!"
+print(message)
+
+
+name = "ada lovelace is a famous computer science engineer. She was born in New york"
+print(name.upper())
+print(name.title())
+print(name.capitalize())
+
+
+
+first_name = "ada"
+last_name = "lovelace"
+full_name = f"{first_name} {last_name}"
+print(full_name)
+
+
+# print(MAX_CONNECTIONS)
+# this is a comment!
